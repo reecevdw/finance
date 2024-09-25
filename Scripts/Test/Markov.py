e791901e76a6e1142a6ebb0d5f88c418b9c931f6
Reece VanDeWeghe <reecevdw@Reeces-MacBook-Pro.local>
2024-09-25 14:01:37 -0500

    fix check

--- Scripts/Test/Markov.py
+++ Scripts/Test/Markov.py
@@ -1,7 +1,22 @@
 
 import numpy as np  
 
-#nth state:
+
+# Check if all rows sum to 1 and all entries are between 0 and 1
+def check_transition(x):
+    x_rows, x_cols = x.shape
+    for r in range(x_rows):
+        row_sum = 0
+        for c in range(x_cols):
+            entry = x[r, c]
+            if entry > 1 or entry < 0:  # Check if entry is between 0 and 1
+                return False
+            row_sum += entry
+        if not np.isclose(row_sum, 1):  # Check if row sums to 1 (using np.isclose to avoid floating-point errors)
+            return False
+    return True
+
+# Nth state:
 def n_state(n, transition, initial):
     P_rows, P_cols = transition.shape
     new = np.identity((P_rows), dtype=np.float64)
@@ -9,7 +24,7 @@ def n_state(n, transition, initial):
         new = np.dot(new,transition)
     return np.dot(new, initial)
 
-#change based on situtaion
+# Change based on situtaion
 S0 = np.matrix('1;0', dtype=np.float64)
 P = np.matrix('0.75, 0.4 ; 0.25, 0.6', dtype=np.float64)
 
