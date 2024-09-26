1c21f616432ccb25098bec75f8b1e8456cafed51
Reece VanDeWeghe <reecevdw@Reeces-MacBook-Pro.local>
2024-09-25 22:22:38 -0500

    fixed

--- Scripts/Test/Markov.py
+++ Scripts/Test/Markov.py
@@ -1,28 +1,29 @@
-
 import numpy as np  
 
-
-# Check if all rows sum to 1 and all entries are between 0 and 1
+# Check if all columns sum to 1 and all entries are between 0 and 1
 def check_transition(x):
     x_rows, x_cols = x.shape
-    for r in range(x_rows):
-        row_sum = 0
-        for c in range(x_cols):
+    for c in range(x_cols):
+        col_sum = 0  # Reset col_sum for each column
+        for r in range(x_rows):
             entry = x[r, c]
             if entry > 1 or entry < 0:  # Check if entry is between 0 and 1
                 return False
-            row_sum += entry
-        if not np.isclose(row_sum, 1):  # Check if row sums to 1 (using np.isclose to avoid floating-point errors)
+            col_sum += entry  # Sum the values in the column
+        if not np.isclose(col_sum, 1):  # Check if column sums to 1 (using np.isclose to avoid floating-point errors)
             return False
     return True
 
 # Nth state:
 def n_state(n, transition, initial):
-    P_rows, P_cols = transition.shape
-    new = np.identity((P_rows), dtype=np.float64)
-    for x in range(n):
-        new = np.dot(new,transition)
-    return np.dot(new, initial)
+    if check_transition(transition):
+        P_rows, P_cols = transition.shape
+        new = np.identity((P_rows), dtype=np.float64)
+        for x in range(n):
+            new = np.dot(new,transition)
+        return np.dot(new, initial)
+    else:
+        return False
 
 # Change based on situtaion
 S0 = np.matrix('1;0', dtype=np.float64)
@@ -31,3 +32,5 @@ P = np.matrix('0.75, 0.4 ; 0.25, 0.6', dtype=np.float64)
 n = input("What State:\t")
 
 print(f"{n} state:\n{n_state(int(n), P, S0)}")
+
+print(f"check: {check_transition(P)}")
